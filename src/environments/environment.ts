8af246345c94abd06bf2ffce794093b09883f14e
mthanki <60077334+mthanki@users.noreply.github.com>
2021-05-28 04:09:04 -0400

    footer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  url: 'http://localhost:8000/api'
+  url: 'https://vault-nodejs.herokuapp.com/api'
+  // url: 'http://localhost:8000/api'
 };
 
 /*
