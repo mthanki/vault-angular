a79ac64c60e95b5070d42209ddf819f16f02494b
mthanki <60077334+mthanki@users.noreply.github.com>
2021-05-31 04:27:59 -0400

    changer for update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate } from '@angular/service-worker';
 import { AuthService } from './auth/auth.service';
 import { DataService } from './http/data.service';
 
@@ -14,9 +16,21 @@ export class AppComponent {
   // avoid circular dependency error
   constructor(
     private authService: AuthService,
-    private dataService: DataService) {
+    private dataService: DataService,
+    updates: SwUpdate,
+    private _snackbar: MatSnackBar) {
 
     this.dataService.setAuthService(this.authService);
+
+
+    updates.available.subscribe(event => {
+      let snackBarRef = this._snackbar.open("New Version Available", "Update")
+      snackBarRef.onAction().subscribe(() => {
+        document.location.reload();
+      });
+    });
+
+
   }
 
 
